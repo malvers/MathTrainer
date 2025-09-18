d1283da1d3827f7126f15e49b5b62e717ae444f6
Dr. Michael R. Alvers <m.alvers@ganata.ownprovider.com>
2025-09-18 16:54:02 +0200

    random name picker distance 4 between re-picks

--- src/mathtrainer/RandomNamePicker.java
+++ src/mathtrainer/RandomNamePicker.java
@@ -15,19 +15,25 @@ import java.util.Random;
 public class RandomNamePicker implements NativeKeyListener {
 
     private final ArrayList<OneStudent> names;
+    private ArrayList<OneStudent> availableNames; // Pool for current round
     private final Random random;
     private volatile boolean running = true;
     private static Clip clip;
+    private int roundNumber = 1;
 
     public RandomNamePicker(ArrayList<OneStudent> names) {
 
-        System.out.println("RandomNamePicker");
+        //System.out.println("RandomNamePicker");
 
-        this.names = names;
+        this.names = new ArrayList<>(names);
+        this.availableNames = new ArrayList<>(names); // Copy for available pool
         this.random = new Random();
 
         setupNativeHook();
         startKeepAlive(); // Start the thread that keeps JVM alive
+
+        System.out.println("Total students: " + names.size());
+        System.out.println("Round 1 started - " + availableNames.size() + " students available");
     }
 
     private void setupNativeHook() {
@@ -109,12 +115,23 @@ public class RandomNamePicker implements NativeKeyListener {
 
     protected void pickRandomName() {
 
-        //System.out.println("pickRandomName: ");
-        OneStudent randomStudent = new OneStudent("no name");
-        if (!names.isEmpty()) {
-            randomStudent = names.get(random.nextInt(names.size()));
-            setAndPlaySound(randomStudent.name);
-            System.out.println("Selected: " + randomStudent.name);
+        if (availableNames.isEmpty()) {
+            // Reset the pool when everyone has been picked
+            availableNames = new ArrayList<>(names);
+            roundNumber++;
+            System.out.println("\n--- Round " + roundNumber + " started! Everyone available again ---");
+            System.out.println("Students available: " + availableNames.size());
+        }
+
+        OneStudent randomStudent = availableNames.remove(random.nextInt(availableNames.size()));
+        setAndPlaySound(randomStudent.name);
+
+        System.out.println("\nSelected: " + randomStudent.name);
+        System.out.println("Round: " + roundNumber);
+        System.out.println("Students left this round: " + availableNames.size());
+
+        if (availableNames.isEmpty()) {
+            System.out.println("--- Round " + roundNumber + " completed! ---");
         }
     }
 
@@ -166,8 +183,22 @@ public class RandomNamePicker implements NativeKeyListener {
         students.add(new OneStudent("Charlie"));
         students.add(new OneStudent("Diana"));
         students.add(new OneStudent("Eve"));
+        students.add(new OneStudent("Lea"));
+        students.add(new OneStudent("Tim"));
+        students.add(new OneStudent("Fritz"));
+        students.add(new OneStudent("Frederike"));
+        students.add(new OneStudent("Eva"));
+        students.add(new OneStudent("Peter"));
+        students.add(new OneStudent("Nic"));
+        students.add(new OneStudent("Edgar"));
+        students.add(new OneStudent("Erwin"));
+        students.add(new OneStudent("Frederik"));
+        students.add(new OneStudent("Lysann"));
+        students.add(new OneStudent("Sarina"));
+        students.add(new OneStudent("Ellen"));
+        students.add(new OneStudent("Aurora"));
+        students.add(new OneStudent("Laura"));
 
-        // This will now keep running automatically!
         new RandomNamePicker(students);
 
         // Main thread can exit immediately - the keep-alive thread keeps JVM running
